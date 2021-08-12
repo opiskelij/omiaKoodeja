c433d261de433caa43ea94ccaff269a8bc3b190d
opiskelij <85615903+opiskelij@users.noreply.github.com>
2021-08-12 21:14:47 +0300

    Add files via upload

---
+++ omat.py
@@ -0,0 +1,284 @@
+
+
+
+def seloste():
+ print("Pelin tarkoituksena on kerätä tietoa, tieto ei tarkoita sitä,")
+ print("että kartoitetaan mitä tyhmempiä asioita, vaikka ne ovatkin tietoa")
+ print("Pelin ideana on rakentaa oma alusta tiedolle ja kuten sanonta")
+ print("kuulostaakin niin tutulta, että tieto lisää tuskaa, on ehken")
+ print("syytä korjata tämä väite. Tieto lisää tuskaa vain sen takia")
+ print("koska ei ymmärrä, ja tämän tarkoituksena on tarjota tietoa")
+ print("eikä niinkään selitettä sille, mikä siihen johti, vaikka")
+ print("oikeita kysymyksiä kysymällä pääseekin pitkälle, on totuus jossain")
+ print("muualla, tämän tarkoitus on luoda fakta pohjainen peli, joka")
+ print("koostuu muistoista, muistista ja tutkitusta tiedosta. Jokaisella")
+ print("on oma ymmärryksensä mitä tieto on, mutta harva osaa sitä lopulta")
+ print("hyödyntää. Kuvittele tilanne jossa kysyt Googlelta kysymyksiä")
+ print("ja saat niihin vastauksia, paljonko kello on, missä on mikäkin")
+ print("Periaate on täysin samanlainen, mutta kunnioittaen jokaista")
+ print("niin yksityisyyttä, kuin oikeutta jakaa itse haluamansa tiedon")
+ print("ilman, että siihen painostetaan tai pakoitetaan. On tarkistajia")
+ print("ja itse oppineita jotka pystyvät ehken sanoa tarkalleen, onko")
+ print("kyseinen tieto totta? Tämän tarkoituksena ei ole kertoa tulevaisuutta")
+ print("vaan pikemminkin täsmätietoa siitä mikä sinulla on esimerkiksi kädessä.")
+ print("Tämä koostuu monesta palasesta, missä jokainen osallistuja on samalla")
+ print("tavalla arvokas, kuin toinen, luovuttaen tai toisella nimityksellä,")
+ print("antaen tiedon eteenpäin. Tietoa ei varsinaisesti voi omistaa, mutta")
+ print("sen voi unohtaa, tämän on tarkoitus myös paikata tuota unohduksen")
+ print("tuottamia aukkoja. Tämä on yritys, joka koostuu siihen osallistujista")
+ print("tällä ei ole hintaa, tässä on vain tietoa joka on koottuna yhteen")
+ print("tai useampaan paikkaan sitä tarvitseville. Voit vapaasti käyttää")
+ print("tämän yritys pelin tuottamaa tietoa, mutta toivoen sinun myös")
+ print("jakavan sitä. Jaat sen mitä joku muu ei ole vielä jakanut tai")
+ print("olet jakamatta. Kuka tästä hyötyy? Sinä, minä, hän, me, te, he, joten")
+ print("miksi tätä voi käyttää ja kuka siitä hyötyy? Tämän tarkoitus on,")
+ print("tarjota tekemistä, tämän tarkoitus on myös oppia jotain uutta")
+ print("tämän tarkoitus on myös paikata muistia ja ennen kaikkea tämän")
+ print("tarkoitus on tuoda ihmiset takaisin yhteen ja lisätä kuuluvuuden")
+ print("tunnetta johonkin, tämä on yhteinen projekti")
+ print("Siinä missä sinulle tämä voi olla tietoa, on tämä minulle eräänlainen")
+ print("koulu / oppiminen. Toivoen kumminkin, että tämä vielä tulee edistymään.")
+ print("Tämä ei ole salaisuus, tietoa ei varsinaisesti pidä salata, mutta")
+ print("on ehken kohtuullisempaa miettiä, missä kohtaan sitä tietoa tulisi käyttää")
+ print("")
+ print("")
+ print("Ystävällisin terveisin Kara")
+ main()
+
+
+
+def omatoiminta():
+ print("Print koodit.")
+ print("C koodi - 1")
+ print("C++ koodi - 2")
+ print("Arduino koodi - 3") 
+ print("Python koodi - 4")
+ print("Binääriä - 5")
+ valinta = input()
+
+ if valinta == '1':
+
+  print("C++ koodia kirjoittaessa tulee ensimmäiseksi print komento. Print komennolla")
+  print("tulostetaan tekstiä ulos, niin että esimerkiksi komentokehotteessa teksti,")
+  print("näyttäytyy sellaisenaan, kuin se on siihen kirjoitettu. Seuraavan laisesti")
+  print("\n#include <iostream>")
+  print("using namespace std;")
+  print("int main(){")
+  print("cout << ''Tämähän toimii! Loistavaaaa!!!!!;''")
+  print(")}")
+  print("\n")
+  print("Tällainen on yleensäkkin ensimmäinen ohjelma jota kirjoitetaan ja tämä luonnollisesti")
+  print("antaa seuraavan tulostuksen ulos\n\n")
+  print("Tämähän toimii! Loistavaaaa!!!!!")
+
+  print("\n\nTätä samaista koodia voidaan osittain käyttää myös arduinon kanssa, mikä on\n"
+        "omien projektien ja tiettyjen mikrokontrollien ansiosta erittäin kannattavaa\n"
+        "lähinnä vain sillä, kuinka paljon se voi opettaa yleisestä elektroniikasta ja\n"
+        "kuinka paljon tuo kapistus on minua opettanut, joten jaan sen myös tähän.\n\n")
+  print("void main(){\n\n}\n\nvoid loop(){\n\nSerial.print(''Tämähän toimii! Loistavaaaa!!!!!''")
+  print("\n}\n\nKuten huomaat, pelkkää c++ koodia kirjoitetaan eri tavalla, mutta kun kyse on\n"
+        "arduinosta, niin se miten se esitetään on aivan toisenlainen.")
+  print("\n\nTämä oli siis arduinoa ja arduinon oma koodi miten se kirjoitetaan. Kirjoitetaan\n"
+        "tähän samaiseen väliin myös miten samainen ohjelma kirjoitetaan pelkällä C:llä ja \n"
+        "kieli jolla nyt kirjoitan ''python''")
+  print("\n\n"
+        "#include <stdio.h>\n"
+        "int main(){\n"
+        "printf{''Tämähän toimii! Loistavaaaa!!!!!''}\n"
+        "return 0;\n"
+        "}\n\n"
+        "Samainen ohjelma voidaan myös kirjoittaa pythonilla mikä taas tarvitsee oman tulkkinsa\n"
+        "vain sen takia, ettei sitä tueta täysin natiivina iha jokaisessa paikassa\n"
+        "\nprint(''Tämähän toimii! Loistavaaaa!!!!!)")
+  print("\n\nNäin kirjoitetaan c++, arduino, c ja python koodit, aloittaen printtaamisesta")
+
+ elif valinta == '2': # c++ aloita
+  print("Muistilaput toimivat näin\n"
+           "\n#include <iostream>\nusing namespace std;\n\nint main(){\n#Tähämä on se "
+     "miten muistilappu toimii")
+
+
+
+  print("Toinen mitä opetetaan heti toiseksi on miten ohjelman saa kirjoittamaan sitä\n"
+        "minkä haluaa sen kirjoittavan eli, c++ käyttäen tulee käyttää cin valintaa.\n"
+        "hypätään esittelyt ja aloitetaan.\n\n"
+        "#include <iostream>\n"
+        "using namespace std;\n\n"
+        "int main(){\n"
+        "int testi;\n"
+        "cout << ''Kirjoitetaan tähän jotain ja kysytään jotain, mutta tässä kohtaa numero'';\n"
+        "cin >> testi;\ncout << testi;\n}")
+  print("\n\n")
+  print("Nähätävästi arduinossa ei ole kyseistä funktiota\n\n"
+         "sama kirjoitettuna sitten pelkällä c:llä on\n"
+         "\n"
+         "#include <stdio.h>\n"
+         "int main(){\n"
+         "\n int i;\nprintf{''tähän sitä tekstiä mitä kysytään''}\n"
+         "scanf(''%d'', &i);\n"
+         "printf(''tähän se vastaus minkä annoit: %d'', + i);\n"
+         "return 0;\n"
+         "}\n"
+         "\nKyseinen ohjelma kirjoittaa ''tähän sitä tekstiä mitä kysytään ja numeron jonka kirjoitat.\n"
+         "se ei lopulta ole kovin ihmeellistä, mutta huomaa kuinka muodot ovat erillaiset.\n\n"
+         "Sama kirjoitettuna sitten pythonilla\n\n"
+         "print(''tähän sitä teksitä'')\n")
+  print("kysymys = input(''mikä kysymys'')\n")
+  print("print(kysymys)\n\n")
+  print("näin kirjoittaa pythoni sen mitä kirjoitat")
+
+ elif valinta == '3':   #arduino aloita
+  print("Harjoitustehtävä 3. - 1")
+  valinta2 = input()
+  if valinta2 == '1':
+        print("Ohjelma joka sytyttää ledit vuorotellen, käyttäen kahta lediä.\n"
+              "#int ledi1;\n"
+              "#int ledi2;\n"
+              "\n"
+              "void main(){\n"
+              "pinMode(ledi1, OUTPUT);\n"
+              "pinMode(ledi2, OUTPUT);\n"
+              "\n"
+              "}\n"
+              "void loop(){\n"
+              "digitalWrite(ledi1, HIGH);\n"
+              "delay(200);\n"
+              "digitalWrite(ledi2, LOW);\n"
+              "delay(200);\n"
+              "digitalWrite(ledi1, LOW);\n"
+              "delay(200);\n"
+              "digitalWrite(ledi2, HIGH);\n"
+              "delay(200)\n"
+              "}\n")
+
+        print("\n\nOhjelma joka sysyttää kolme kertaa saman ledin ja neljännellä molemmat ledit\n"
+            "#int ledi1;\n"
+            "#int ledi2;\n"
+            "\n"
+            "void main(){\n"
+            "pinMode(ledi1, OUTPUT);\n"
+            "pinMode(ledi2, OUTPUT);\n"
+            "}\n"
+            "void loop(){\n"
+            "\n"
+            "digitalWrite(")
+
+
+
+
+
+
+
+ 
+ elif valinta == '4': # tee koodilla
+     print("Pyyttoni")
+
+ elif valinta == '4': #binäärit
+     print("binääriä")
+ 
+def main():
+
+
+ print("KKKKK     KKKKK KKKKK          KKKKK MMMMMMMMMM            MMMMMMMMMM")
+ print("KKKKK    KKKKK   KKKKK        KKKKK  MMMMM MMMMM          MMMMM MMMMM")
+ print("KKKKK   KKKKK     KKKKK      KKKKK   MMMMM  MMMMM        MMMMM  MMMMM")
+ print("KKKKK  KKKKK       KKKKK    KKKKK    MMMMM   MMMMM      MMMMM   MMMMM")
+ print("KKKKK KKKKK         KKKKK  KKKKK     MMMMM    MMMMM    MMMMM    MMMMM")
+ print("KKKKKKKKK             KKKKKKKkK      MMMMM     MMMMM  MMMMM     MMMMM")
+ print("KKKKK KKKKK             KKKKK        MMMMM      MMMMMMMMMM      MMMMM")
+ print("KKKKK  KKKKK            KKKKk        MMMMM                      MMMMM")
+ print("KKKKK   KKKKK           KKKKk        MMMMM                      MMMMM")
+ print("KKKKK    KKKKK          KKKKk        MMMMM                      MMMMM")
+ print("KKKKK      KKKKK        MMMMM        MMMMM                      MMMMM")
+
+
+ print("Seloste - 1")
+ print("Oma toiminta - 2")
+
+ kysymys = input("Valinta - ")
+ if kysymys == '1':
+  seloste()
+ elif kysymys == '2':
+  omatoiminta()
+
+ elif kysymys == '3':
+  print("Terverhdys! ")
+  kayttis = input("Käyttäjänimi: ")
+  sana = input("Avain 1: ")
+  sana2 = input("Avain 2: ")
+  sana3 = input("Avain 3: ")
+  sana4 = input("Avain 4: ")
+  sana5 = input("Avain 5: ")
+
+  if kayttis == "vilho":
+   if (sana == "moi1" and sana2 == "moi2" and sana3 == "moi3" and sana4 == "moi4" and sana5 == "moi5"):
+    print("Toimii")
+
+  elif kayttis == "toinen":
+   if (sana == "toinen1" and sana == "toinen2" and sana == "toinen3" and sana == "toinen4" and sana == "toinen5"):
+    print("Toimii myös")
+
+  elif kayttis == "kolmas":
+   if (sana == "kolmas1" and sana == "kolmas" and sana == "kolmas3" and sana == "kolmas4" and sana == "kolmas5"):
+    print("Toimii se tämäkin")
+
+  elif kayttis == "neljäs":
+   if (sana == "neljäs1" and sana == "neljäs2" and sana == "neljäs3" and sana == "neljäs4" and sana == "neljäs5"):
+    print("toimii se tämäkin")
+
+  else:
+   print("kokeile uudelleen")
+
+
+if __name__ == "__main__":
+ main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print("Yleensä koodia kirjoittaessa tulisi joko itsensä tai muiden takia kirjoittaa mahdollisesti koodin päälle\n"
+#           "tai koodin alle tekstiä siitä mitä koodi tekee. Tietyt ilmaisut ovat helpoin kommunikointi henkilön kanssa\n"
+#           "joka kirjoittaa koodia sinun jälkeesi. Se on eräänlainen muistilappu siitä, miten koodi toimii.\n"
+#           "On toki totta, että unohdan itsekkin kyseiset muistutukset vain olettaen, että kirjoitan koodia itse,\n"
+#           "mutta sitä suuremmallakin syyllä olisi hyvä kirjoittaa ylös mitä koodi tekee. Muistilaput toimivat näin\n"
+#           "\n#include <iostream>\nusing namespace std;\n\nint main(){\n#Tähämä on se miten muistilappu toimii\n"
+#           "#kyseinen koodi siis kirjoittaa ulostulon jonka jälkeen se kysyy numeroa jonka se sitten tulostaa ulos\n"
+#           "int numero;\ncout << ''Mikä on lempinumerosi?'';\ncin << numero;\ncout << numero;\n}\n\n"
+#           "Risuaita # siis kuvastaa sitä osuutta joka ei näy koodissa itsessään, mutta editorilla sen voi nähdä.\n\n"
+#           "Tämä samainen tyyli soveltuu jokaiseen c koodiin. Pythonilla toimii sama rakenne.")
